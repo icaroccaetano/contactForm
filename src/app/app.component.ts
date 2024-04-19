f188027565b11c3ba287d9fea88a2571e08f1e6c
icaroccaetano <icaroccaetano@gmail.com>
2024-04-19 16:11:08 -0300

    error correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,10 +12,4 @@ import { RouterOutlet } from '@angular/router';
 
 export class AppComponent {
   title = 'contact-form';
-}
-
-export class ContactFormComponent {
-  log(x) {
-    console.log(x);
-  }
 }
